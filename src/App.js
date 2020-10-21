6f69097cb3710cba5c4706463b111901d04ac902
jawanAlhulail <Jawanalhulail@gmail.com>
2020-10-22 01:21:57 +0300

    register done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,67 @@
-import React from "react";
-import HomePage from "./Pages/HomePage.jsx";
+import React from 'react';
+import LoginPage from "./Pages/Login"
+import './App.css';
+import RegisterPage from "./Pages/RegisterPage"
+import { BrowserRouter, Route } from "react-router-dom";
+import ProfilePage from "./Pages/ProfilePage"
+import HomepagePAGE from './Pages/homepagePAGE';
 
-function App() {
+ 
+class App extends React.Component {
+  
+  state={
+    response:"",
+    value: "",
+      username: "",
+      email:"",
+      password:"",
+  }
+
+  
+  render(){
+   
   return (
-    <div className="App">
-      <HomePage />
-    </div>
-  );
+    <>
+    <BrowserRouter>
+     <Route
+            exact
+            path="/register"
+            render={(props) => (
+              <RegisterPage />
+            )}
+          />
+           <Route
+            exact
+            path="/login"
+            render={(props) => (
+              <LoginPage  />
+            )}
+            
+
+          />
+           <Route
+            exact
+            path="/profile"
+            render={(props) => (
+              <ProfilePage  />
+            )}
+            
+
+          />
+          <Route
+            exact
+            path="/homepage"
+            render={(props) => (
+              <HomepagePAGE  />
+            )}
+            
+
+          />
+    </BrowserRouter>
+           
+    </>
+    
+  ); }
 }
 
 export default App;
---
+++ src/Pages/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from "../Components/Login"
+class LoginPage extends React.Component{
+    render(){
+        return (
+            <>
+            <Login/>
+            </>
+        )
+            
+        
+    }
+}
+export default LoginPage
